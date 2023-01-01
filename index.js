183dce34909f9af5bcd88a0f90fb2a6a3f799ac6
tahir hussian <tahir.toorie72@gmail.com>
2023-01-01 15:16:16 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+const_p_btns = document.querySelector('.p-btns');
+const_p_btn = document.querySelectorAll('');
+const_p_img_elem = document.querySelectorAll('.p-btn');
+p_btns.addEventListner('click ,(e)')
