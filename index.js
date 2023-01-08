03c69cd46984826b0c3fc9e91dc5261d92c0835c
tahir hussian <tahir.toorie72@gmail.com>
2023-01-08 21:58:19 +0500

    final stage

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const p_btns = document.querySelector(".p-btns");
-const p_btn = document.querySelectorAll(".p-btn");
-const p_img_elem = document.querySelectorAll(".img-overlay");
-p_btns.addEventListener('click', (e)=>{
-   const p_btn_clicked = e.target;
-//    console.log(p_btn_clicked);
-   p_btn.forEach((curElem) => curElem.classList.remove("p-btn-active"));
-   p_btn_clicked.classList.add("p-btn-active");
-   const btn_num = p_btn_clicked.dataset.btnNum;
-//    console.log(btn_num);
-   const img_active = document.querySelectorAll(`.p-btn--${btn_num}`);
-   p_img_elem.forEach((curElem) => curElem.classList.add("p-image-not-active"));
-   img_active.forEach((curElem) => curElem.classList.remove("p-image-not-active"));
+// var p_btns = document.querySelector(".p-btns");
+// var p_btn = document.querySelectorAll(".p-btn");
+// const p_img_elem = document.querySelectorAll(".img-overlay");
+// p_btns.addEventListener('click', (e)=>{
+//    const p_btn_clicked = e.target;
+// //    console.log(p_btn_clicked);
 
-});
+// if(!p_btn_clicked.classList.contains("p-btn")) return ;
+//    p_btn.forEach((curElem) => curElem.classList.remove("p-btn-active"));
+//    p_btn_clicked.classList.add("p-btn-active");
+//    const btn_num = p_btn_clicked.dataset.btnNum;
+// //    console.log(btn_num);
+//    const img_active = document.querySelectorAll(`.p-btn--${btn_num}`);
+//    p_img_elem.forEach((curElem) => curElem.classList.add("p-image-not-active"));
+//    img_active.forEach((curElem) => curElem.classList.remove("p-image-not-active"));
+
+// });
 
 // our testimonail start here
 
@@ -90,7 +92,7 @@ counterNum.forEach((curElem) =>{
 
 // to create response navbar component
 
-const mobile_nav = document.querySelector(".mobile-navbar-btn");
+var mobile_nav = document.querySelector(".mobile-navbar-btn");
 const headerElem = document.querySelector(".header");
 
 mobile_nav.addEventListener("click", ()=>{
